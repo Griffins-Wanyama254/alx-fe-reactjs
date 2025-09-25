08b2278758d7455642efcd229f6f02a361b20dd2
Griffins-Wanyama <wanyamagriffins20@gmail.com>
2025-09-26 00:54:54 +0300

    Add inline styled components in alx-react-app-new project

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+  return (
+    <main style={{ backgroundColor: '#f0f8ff', padding: '15px', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
